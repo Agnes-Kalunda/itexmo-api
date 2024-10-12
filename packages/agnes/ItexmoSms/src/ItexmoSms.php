ddf902e6be856eab2eac29bbd61ee9334bb0bebe
Agnes <agypeter97@gmail.com>
2024-10-13 01:49:06 +0300

    added sendSMS function to send SMS using Guzzle's POST method

--- packages/agnes/ItexmoSms/src/ItexmoSms.php
+++ packages/agnes/ItexmoSms/src/ItexmoSms.php
@@ -13,6 +13,21 @@ class ItexmoSms{
         $this->client =new Client(['base_uri' =>'']);
         $this->apicode= Config::get('itexmo.api_code');
     }
+
+    public function sendSMS($number, $message){
+        $response = $this->client->post('',[
+            'form_params'=>[
+                '1'=> $number,
+                '2'=> $message,
+                '3'=>$this->apicode,
+            ],
+        ]);
+
+        $result =$response->getBody()->getContents();
+
+        return $this->handleResponse($result);
+
+    }
 }
 
 
