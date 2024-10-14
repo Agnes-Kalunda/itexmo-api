85759af4deeaa629246ff21f7980e9e515c4dc86
Agnes <agypeter97@gmail.com>
2024-10-14 14:41:12 +0300

    update config file to include needed needed variables

--- packages/agnes/ItexmoSms/config/itexmo.php
+++ packages/agnes/ItexmoSms/config/itexmo.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
-    'api_key' => env('ITEXMO_API_KEY', ''),
-    'api_url' => env('ITEXMO_API_URL', ''),
+    'api_url' => env('ITEXMO_API_URL', 'https://api.itexmo.com/api'),
+    'email' => env('ITEXMO_EMAIL', ''),
+    'password' => env('ITEXMO_PASSWORD', ''),
+    'api_code' => env('ITEXMO_API_CODE', ''),
 ];
